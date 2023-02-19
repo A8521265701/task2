61ae9b81676c6b5dd53da78bb231342e39220bcd
ABHISHEK KUMAR <103755594+A8521265701@users.noreply.github.com>
2023-02-19 19:39:59 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,47 @@
-let header = document.querySelector("header");
-let menu = document.querySelector("#menu-icon");
-let navbar = document.querySelector(".navbar");
+const converter = document.querySelector('#converter');
+const result = document.querySelector('.result');
+const convertButton = document.querySelector('.convertButton');
+const resetButton = document.querySelector('.resetButton');
+const changeButton = document.querySelector('.changeButton');
+const C = document.querySelector('.C')
+const F = document.querySelector('.F')
 
-window.addEventListener("scroll", () => {
-  header.classList.toggle("shadow", window.scrollY > 0);
-});
-
-menu.onclick = () => {
-  navbar.classList.toggle("active");
-};
-window.onscroll = () => {
-  navbar.classList.remove("active");
-};
-
-// Dark Mode / light mode
-let darkmode = document.querySelector("#darkmode");
-
-darkmode.onclick = () => {
-  if (darkmode.classList.contains("bx-moon")) {
-    darkmode.classList.replace("bx-moon", "bx-sun");
-    document.body.classList.add("active");
-  } else {
-    darkmode.classList.replace("bx-sun", "bx-moon");
-    document.body.classList.remove("active");
-  } 
+const swap = () => {
+    if (C.innerHTML === '°C') {
+        C.innerHTML = '°F';
+        F.innerHTML = '°C';
+        converter.placeholder = "Temperature in °F";
+    }else {
+        F.innerHTML = '°F';
+        C.innerHTML = '°C';
+        converter.placeholder = "Temperature in °C";
+    };
 };
 
+const reset = () => {
+    converter.value = '';
+    result.innerHTML = '';
+}
+
+const convert = () => {
+    if (/^(-?)(\d+)(\.{1}\d+)?$/.test(converter.value)) {
+        result.style.color = "black"
+        if (C.innerHTML === '°C') {
+            let value = (converter.value * 1.8) + 32;
+            result.innerHTML = `${converter.value}°C is equal to ${value.toFixed(2)}°F`;
+        } else {
+            let value = (converter.value - 32) / 1.8;
+            result.innerHTML = `${converter.value}°F is equal to ${value.toFixed(2)}°C`;
+        }
+    } else if (converter.value == '') {
+        result.style.color = "#993300"
+        result.innerHTML = 'Enter some number';
+    } else {
+        result.style.color = "#993300"
+        result.innerHTML = 'Enter only numbers!!!';
+    }
+}
+
+changeButton.addEventListener('click', swap);
+resetButton.addEventListener('click', reset);
+convertButton.addEventListener('click', convert);
